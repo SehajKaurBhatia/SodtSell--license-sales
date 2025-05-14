98e99d744b6943394246756aa752836a425821c4
SehajKaurBhatia <sebhat1504@gmail.com>
2025-05-14 16:41:49 +0530

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-     
+     <link rel="icon" href="/doc.png"/>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
